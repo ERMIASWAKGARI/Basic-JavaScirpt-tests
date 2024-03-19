745be04213eb2b5262f255d56a5113acee266dcb
ERMIASWAKGARI <ermiaswakgarisse5@gmail.com>
2024-03-19 20:32:16 +0300

    Task -3: DOM - Toggle Visibility

--- Basic-JavaScirpt-tests/DOM Manipulation/toggleVisibility.js
+++ Basic-JavaScirpt-tests/DOM Manipulation/toggleVisibility.js
@@ -1,5 +1,5 @@
 document.getElementById("toggleButton").addEventListener("click", function () {
-  var contentDiv = document.getElementById("content");
+  const contentDiv = document.getElementById("content");
   if (contentDiv.style.display === "none") {
     contentDiv.style.display = "block";
   } else {
